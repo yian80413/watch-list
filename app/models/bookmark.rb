039a0d99b6622112722fdd825a51b7e2a152042d
Yi-An Ko <yian80413@gmail.com>
2021-11-12 11:00:33 +0100

    updated

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
+  validates :comment, length: { minimum: 5 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
+    resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
+
+#   GET "lists/42"
+# A user can create a new list
+# GET "lists/new"
+# POST "lists"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
